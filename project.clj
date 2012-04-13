8f5962b85fa701e0f484308677c7a6b7a07a10c3
Sun Ning <classicning@gmail.com>
2012-04-13 21:21:31 +0800

    add project.clj

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject slacker/slacker-cluster "0.8.0-SNAPSHOT"
+  :description "Cluster support for slacker"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [slacker "0.8.0-SNAPSHOT"]
+                 [zookeeper-clj "0.9.2"
+                  :exclusions [jline junit]]
+                 [org.clojure/tools.logging "0.2.3"]]
+  :dev-dependencies [[codox "0.6.1"]]
+  :extra-classpath-dirs ["examples"]
+  :run-aliases {:cluster-server "slacker.example.cluster-server"
+                :cluster-client "slacker.example.cluster-client"})
+
