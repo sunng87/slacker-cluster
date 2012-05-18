5f10fd712f20ae747405b73f585f81c224497fdc
Sun Ning <classicning@gmail.com>
2012-05-19 00:14:09 +0800

    bump to 0.8.2

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject slacker/slacker-cluster "0.8.0"
+(defproject slacker/slacker-cluster "0.8.2"
   :description "Cluster support for slacker"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [slacker "0.8.0"]
+                 [slacker "0.8.2"]
                  [zookeeper-clj "0.9.2"
                   :exclusions [jline junit]]
                  [org.clojure/tools.logging "0.2.3"]]
