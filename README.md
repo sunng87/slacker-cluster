111832da491076d2a2781955d994c89d7a66e5b5
Sun Ning <nsun@avos.com>
2012-11-01 11:01:15 +0800

    bump version

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ instance in your system.
 
 ## Leiningen
 
-`[slacker/slacker-cluster "0.8.4.2"]`
+`[slacker/slacker-cluster "0.8.5"]`
 
 ## Cluster Enabled Slacker Server
 
--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject slacker/slacker-cluster "0.8.4.2"
+(defproject slacker/slacker-cluster "0.8.5"
   :description "Cluster support for slacker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [slacker "0.8.4"]
+                 [slacker "0.8.5"]
                  [zookeeper-clj "0.9.2"
                   :exclusions [jline junit]]
                  [org.clojure/tools.logging "0.2.3"]]
