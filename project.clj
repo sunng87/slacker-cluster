c8a4fc4f97d6c4d3df8795d5633391389a6b5776
Sun Ning <nsun@avos.com>
2014-02-23 19:41:59 +0800

    added url in project

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Cluster support for slacker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/sunng87/slacker-cluster"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [slacker "0.11.0-SNAPSHOT"]
                  [zookeeper-clj "0.9.3"
