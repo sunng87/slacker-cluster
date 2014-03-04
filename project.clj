629d40c9a502d2bd3db5e66953553255fa5ba23d
Sun Ning <nsun@avos.com>
2014-03-04 17:54:59 +0800

    bring back log4j as a compile dependency

--- project.clj
+++ project.clj
@@ -6,10 +6,9 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [slacker "0.11.0-SNAPSHOT"]
                  [zookeeper-clj "0.9.3"
-                  :exclusions [jline junit log4j]]
+                  :exclusions [jline junit]]
                  [org.clojure/tools.logging "0.2.6"]]
   :profiles {:example {:source-paths ["examples"]}
-             :dev {:dependencies [[log4j "1.2.17"]]}
              :1.3 {:dependencies [org.clojure/clojure "1.3.0"]}}
   :plugins [[codox "0.6.7"]]
   :global-vars {*warn-on-reflection* true}
