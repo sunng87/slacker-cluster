6644ce5d7ece7e70b40a13b822f17a1655a9480d
Sun Ning <nsun@avos.com>
2014-03-06 10:20:54 +0800

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slacker/slacker-cluster "0.11.0"
+(defproject slacker/slacker-cluster "0.11.1"
   :description "Cluster support for slacker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
