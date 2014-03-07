0e3eb4c0123809438e78a7bb70d02bd7452c8a43
Sun Ning <nsun@avos.com>
2014-03-07 17:12:43 +0800

    there's an issue that the zookeeper connection between slacker server and zookeeper is gone, without any attempt to reconnect. This is strange so I update zookeeper to latest (two years ago) release to see if it could fix.

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
   :url "https://github.com/sunng87/slacker-cluster"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [slacker "0.11.1-SNAPSHOT"]
-                 [zookeeper-clj "0.9.3"
+                 [zookeeper-clj "0.9.3"]
+                 [org.apache.zookeeper/zookeeper "3.4.5"
                   :exclusions [jline junit]]
                  [org.clojure/tools.logging "0.2.6"]]
   :profiles {:example {:source-paths ["examples"]}
