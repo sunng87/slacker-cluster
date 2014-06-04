dc2c7cf8f251374f217823c635b678c4ddfbf66b
Sun Ning <nsun@avoscloud.com>
2014-06-04 16:18:27 +0800

    bump version

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject slacker/slacker-cluster "0.11.5"
+(defproject slacker/slacker-cluster "0.11.6"
   :description "Cluster support for slacker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/sunng87/slacker-cluster"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [slacker "0.11.2"]
+                 [slacker "0.11.5"]
                  [zookeeper-clj "0.9.3"]
                  [org.apache.zookeeper/zookeeper "3.4.5"
                   :exclusions [jline junit]]
