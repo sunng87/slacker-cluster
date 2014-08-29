1f15c045f88930bec4c1edcc824836359b9fbbbe
Sun Ning <sunng@about.me>
2014-08-29 17:56:57 +0800

    update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slacker/slacker-cluster "0.11.6"
+(defproject slacker/slacker-cluster "0.11.7-SNAPSHOT"
   :description "Cluster support for slacker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -9,8 +9,7 @@
                  [org.apache.zookeeper/zookeeper "3.4.5"
                   :exclusions [jline junit]]
                  [org.clojure/tools.logging "0.2.6"]]
-  :profiles {:example {:source-paths ["examples"]}
-             :1.3 {:dependencies [org.clojure/clojure "1.3.0"]}}
+  :profiles {:example {:source-paths ["examples"]}}
   :plugins [[codox "0.6.7"]]
   :global-vars {*warn-on-reflection* true}
   :aliases {"run-example-server" ["with-profile" "default,example" "run" "-m" "slacker.example.cluster-server"]
