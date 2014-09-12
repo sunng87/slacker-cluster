a62a9f1d59324672a94eb7880df0e5cfaa464367
Sun Ning <sunng@about.me>
2014-09-12 14:12:53 +0800

    bump slacker version

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/sunng87/slacker-cluster"
-  :dependencies [[slacker "0.12.2"]
+  :dependencies [[slacker "0.12.3"]
                  [org.apache.curator/curator-framework "2.6.0"
                   :exclusions [jline]]
                  [org.apache.curator/curator-recipes "2.6.0"]
