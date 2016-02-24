22d7a947be518bda2266d618cde5637455d96a2d
Ning Sun <sunng@about.me>
2016-02-24 18:58:45 +0800

    (chore) update dependencies

--- project.clj
+++ project.clj
@@ -8,14 +8,14 @@
                  [org.apache.curator/curator-recipes "2.9.0"]
                  [org.clojure/tools.logging "0.3.1"]]
   :profiles {:example {:source-paths ["examples"]}
-             :dev {:dependencies [[org.clojure/clojure "1.7.0"]
-                                  [slacker "0.13.0"]
+             :dev {:dependencies [[org.clojure/clojure "1.8.0"]
+                                  [slacker "0.14.0-SNAPSHOT"]
                                   [log4j "1.2.17"]
                                   [org.slf4j/slf4j-log4j12 "1.7.12"]]}
              :clojure15 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :clojure16 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :clojure17 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :clojure18 {:dependencies [[org.clojure/clojure "1.8.0-beta1"]]}}
+             :clojure18 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
   :plugins [[codox "0.8.15"]]
   :global-vars {*warn-on-reflection* true}
   :aliases {"run-example-server" ["with-profile" "default,dev,clojure17,example" "run" "-m" "slacker.example.cluster-server"]
