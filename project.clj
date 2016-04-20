8e8479f281152f3fb27a5009ffb24e9788f1acdc
Ning Sun <sunng@about.me>
2016-04-20 15:14:07 +0800

    (fix) drop support for clojure 1.5 and 1.6
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -12,14 +12,12 @@
                                   [slacker "0.14.0-SNAPSHOT"]
                                   [log4j "1.2.17"]
                                   [org.slf4j/slf4j-log4j12 "1.7.12"]]}
-             :clojure15 {:dependencies [[org.clojure/clojure "1.5.1"]]}
-             :clojure16 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :clojure17 {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :clojure18 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
   :plugins [[codox "0.8.15"]]
   :global-vars {*warn-on-reflection* true}
   :aliases {"run-example-server" ["with-profile" "default,dev,clojure17,example" "run" "-m" "slacker.example.cluster-server"]
             "run-example-client" ["with-profile" "default,dev,clojure17,example" "run" "-m" "slacker.example.cluster-client"]
-            "test-all" ["with-profile" "default,clojure15:default,clojure16" "test"]}
+            "test-all" ["with-profile" "default,dev,clojure17:default,dev,clojure18" "test"]}
   :deploy-repositories {"releases" :clojars}
   :jvm-opts ["-Xmx256m"])
