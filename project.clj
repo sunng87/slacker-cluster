30fae7a325eb82d6e4458bd77f566bb8d4e167f2
Ning Sun <sunng@about.me>
2016-05-20 23:01:24 +0800

    (chore) update dependencies
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :url "https://github.com/sunng87/slacker-cluster"
-  :dependencies [[org.apache.curator/curator-framework "2.10.0"
+  :dependencies [[org.apache.curator/curator-framework "3.1.0"
                   :exclusions [jline]]
-                 [org.apache.curator/curator-recipes "2.10.0"]
+                 [org.apache.curator/curator-recipes "3.1.0"]
                  [org.clojure/tools.logging "0.3.1"]]
   :profiles {:example {:source-paths ["examples"]}
              :dev {:dependencies [[org.clojure/clojure "1.8.0"]
-                                  [slacker "0.14.0-SNAPSHOT"]
+                                  [slacker "0.14.1"]
                                   [log4j "1.2.17"]
                                   [org.slf4j/slf4j-log4j12 "1.7.21"]]}
              :clojure17 {:dependencies [[org.clojure/clojure "1.7.0"]]}
