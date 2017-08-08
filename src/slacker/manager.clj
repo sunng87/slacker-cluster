87a7a5a4f61e2ba456aa3e12b803769e3c64022f
Ning Sun <sunng@about.me>
2017-08-08 22:58:51 +0800

    (feat) added slacker manager apis
    Signed-off-by: Ning Sun <sunng@about.me>

---
+++ src/slacker/manager.clj
@@ -0,0 +1,12 @@
+(ns slacker.manager
+  (:require [slacker.server.cluster :as ss]))
+
+(defn slacker-manager-api [server]
+  {"slacker.cluster.manager"
+   {"offline" (fn [] (ss/unpublish-all! server))
+    "offline-ns" (fn [nsname] (ss/unpublish-ns! server nsname))
+    "online-ns" (fn [nsname] )
+    "online" (fn [] )
+    "set-server-data!" (fn [data] (ss/set-server-data! server data))
+    "server-data" (fn [] )
+    "shutdown" (fn [] )}})
