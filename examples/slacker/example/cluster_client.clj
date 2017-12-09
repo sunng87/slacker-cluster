ce2c3e8c634a91ffffc0a463dcc9867cbd2fe6ce
Ning Sun <sunng@about.me>
2017-12-09 09:28:04 +0800

    (test) added description to example
    Signed-off-by: Ning Sun <sunng@about.me>

--- examples/slacker/example/cluster_client.clj
+++ examples/slacker/example/cluster_client.clj
@@ -7,19 +7,28 @@
 (def interceptor {:before-merge (fn [results] (println "Before merge:" results) results)
                   :after-merge (fn [result] (println "After merge:" result) result)})
 
-(def sc (clustered-slackerc "example-cluster" "127.0.0.1:2181"
-                            :interceptors interceptor))
+(def sc (clustered-slackerc "example-cluster" "127.0.0.1:2181"))
 
 (use-remote 'sc 'slacker.example.api)
+
+;; def a remote function that returns async promise
 (defn-remote sc async-timestamp
   :remote-name "timestamp"
   :remote-ns "slacker.example.api"
   :async? true)
+
+;; def a remote function that calls on all provides in cluster, and
+;; return a vector of results
 (defn-remote sc all-timestamp
   :remote-name "timestamp"
   :remote-ns "slacker.example.api"
   :grouping :all
   :grouping-results :vector)
+
+;; def a remote function that choose a provide with least pending
+;; requests, and return a map of result, of which key is the address
+;; of provider. Returns a promise, and a callback is registered and
+;; will be called when result is ready.
 (defn-remote sc async-timestamp
   :remote-name "timestamp"
   :remote-ns "slacker.example.api"
@@ -28,6 +37,8 @@
   :async? true
   :callback (fn [e r]
               (println "++++" r)))
+
+;; You can access more then 1 remote namespace.
 (defn-remote sc echo2
   :remote-ns "slacker.example.api2"
   :grouping :random)
@@ -37,7 +48,7 @@
 
   (println (echo 23))
   (println (all-timestamp))
-  (async-timestamp)
+  (println "Async call timestamp:" @(async-timestamp))
   (try (make-error) (catch Exception e (println "Expected exception:" (ex-data e))))
   (println (echo2 "Echo" "Some" "Data"))
 
