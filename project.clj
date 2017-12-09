1d467d40ff10f2cc10fcd07bbfc3ba25595bbfa8
Ning Sun <sunng@about.me>
2017-12-09 11:15:17 +0800

    (chore) start 0.16.0 and bump clojure to 1.9.0
    Signed-off-by: Ning Sun <sunng@about.me>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slacker/slacker-cluster "0.15.1-SNAPSHOT"
+(defproject slacker/slacker-cluster "0.16.0-SNAPSHOT"
   :description "Cluster support for slacker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
@@ -10,12 +10,12 @@
                  [org.clojure/tools.logging "0.4.0"]]
   :profiles {:example {:source-paths ["examples"]
                        :dependencies [[org.apache.curator/curator-test "4.0.0"]]}
-             :dev {:dependencies [[org.clojure/clojure "1.8.0"]
+             :dev {:dependencies [[org.clojure/clojure "1.9.0"]
                                   [slacker "0.16.0-SNAPSHOT"]
                                   [log4j "1.2.17"]
                                   [org.slf4j/slf4j-log4j12 "1.7.25"]]}
              :clojure18 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :clojure19 {:dependencies [[org.clojure/clojure "1.9.0-beta2"]]}}
+             :clojure19 {:dependencies [[org.clojure/clojure "1.9.0"]]}}
   :plugins [[lein-codox "0.9.5"]]
   :global-vars {*warn-on-reflection* true}
   :aliases {"run-example-server" ["with-profile" "default,dev,clojure18,example" "run" "-m" "slacker.example.cluster-server"]
