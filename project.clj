012a9740519ac26bc0408a67125f5f86b52733eb
Ning Sun <sunng@about.me>
2017-12-20 00:55:02 +0800

    Version 0.16.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slacker/slacker-cluster "0.16.0"
+(defproject slacker/slacker-cluster "0.16.1-SNAPSHOT"
   :description "Cluster support for slacker"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
